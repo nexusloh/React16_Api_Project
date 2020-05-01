58b8ca156ac0d1a3dabec9b03f6ccb426d6ebd06
dmitrosh <dmytrosh@default-value.com>
2020-05-01 16:25:29 +0300

    refactor data acquisition functionality

--- src/components/RandomSite/RandomSite.js
+++ src/components/RandomSite/RandomSite.js
@@ -7,22 +7,22 @@ export default class RandomSite extends Component {
     apiService = new ApiService();
 
     state = {
-        site: null,
-        ip: null,
-        activity: null,
-        image: null
+        site: {}
     };
 
+    randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+
+    onSiteUpdate = (site) => {
+        this.setState({ site });
+    }
+
     updateSite() {
-        this.apiService.getSiteById(Math.floor(Math.random()*10)+1)
-            .then((site) => {
-                this.setState({
-                    site: site.site,
-                    ip: site.ip,
-                    activity: site.activity,
-                    image: site.image
-                });
-            });
+        this.apiService.getSiteById(this.randomInt(1, 10))
+            .then(this.onSiteUpdate);
     }
 
     constructor(props) {
@@ -32,7 +32,7 @@ export default class RandomSite extends Component {
     }
 
     render() {
-        const { site, ip, activity, image } = this.state;
+        const { site: { site, ip, activity, image } } = this.state;
 
         return (
             <div className={'random-site-wrapper'}>
@@ -45,7 +45,7 @@ export default class RandomSite extends Component {
                 <div className="site-info ml-3">
                     <p>Site Url: <span>{ site }</span></p>
                     <p>Site Ip: <span>{ ip }</span></p>
-                    <p>Site Activity <span>{ activity }</span></p>
+                    <p>Site Activity: <span>{ activity }</span></p>
                 </div>
             </div>
         );
