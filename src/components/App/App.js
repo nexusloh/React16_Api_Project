0052b2a85ca4089d3db3a75775cfd32bfc386c67
dmitrosh <dmytrosh@default-value.com>
2020-05-01 13:29:26 +0300

    Random Site functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => (
-  <div>
-    App Component
-  </div>
-);
+//components
+import Header from "../Header/Header";
+import ItemsList from "../ItemsList/ItemsList";
+import SitesDetails from "../SitesDetails/SitesDetails";
+import RandomSite from "../RandomSite/RandomSite";
 
-export default App;
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <Header />
+                    <RandomSite />
+
+                    <div className={'row'}>
+                        <div className={'col-md-6'}>
+                            <ItemsList />
+                        </div>
+                        <div className={'col-md-6'}>
+                            <SitesDetails />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+};
